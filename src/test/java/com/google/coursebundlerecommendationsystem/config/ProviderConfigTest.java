6a5da90c830229193be1efb1d3f7e67fd970ad7c
Bartosz Bujnicki <bbujnicki@internal.oculus-tech.com>
2024-11-16 09:50:50 +0100

    Update test to use ProviderLoader in order to speed up the tests

--- src/test/java/com/google/coursebundlerecommendationsystem/config/ProviderConfigTest.java
+++ src/test/java/com/google/coursebundlerecommendationsystem/config/ProviderConfigTest.java
@@ -1,34 +1,36 @@
 package com.google.coursebundlerecommendationsystem.config;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.google.coursebundlerecommendationsystem.model.Provider;
 import com.google.coursebundlerecommendationsystem.model.Topic;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 public class ProviderConfigTest {
 
-    @Autowired
-    List<Provider> providers;
+    ProviderLoader providerLoader;
 
-
-    @Test
-    public void should_LoadProvidersFromTheFileAndTranslateItToProvider(){
-         final int expectedProviderSize = 3;
-
-        assertNotNull(providers, "Providers list can not be null");
-        assertEquals(expectedProviderSize, providers.size());
-        Provider providerA = providers.getFirst();
-
-        assertEquals("provider_a", providerA.getProvider());
-        assertTrue(providerA.getTopics().contains(Topic.MATH), "Provider_a should offer MATH");
-        assertTrue(providerA.getTopics().contains(Topic.SCIENCE), "Provider_a should offer SCIENCE");
+    @BeforeEach
+    void setup() {
+         providerLoader = new ProviderLoader();
     }
 
+    @Test
+    public void should_LoadProvidersFromTheFileAndTranslateItToProvider() {
+        // Act
+        List<Provider> providers = providerLoader.providers();
 
+        // Assert
+        assertNotNull(providers, "Providers list should not be null");
+        assertEquals(3, providers.size(), "Providers list should contain 3 items");
+
+        Provider providerA = providers.getFirst();
+        assertEquals("provider_a", providerA.getProvider());
+        assertTrue(providerA.getTopics().contains(Topic.MATH));
+        assertTrue(providerA.getTopics().contains(Topic.SCIENCE));
+    }
 }

d897e2e6fd82d6f6247758c74a5ca0406c1a97c3
Bartosz Bujnicki <bbujnicki@internal.oculus-tech.com>
2024-11-17 20:05:23 +0100

    Upgrade the code in provider loader in order to keep readability

--- src/main/java/com/google/coursebundlerecommendationsystem/config/ProviderLoader.java
+++ src/main/java/com/google/coursebundlerecommendationsystem/config/ProviderLoader.java
@@ -9,51 +9,58 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ResourceUtils;
 
 @Component
 public class ProviderLoader {
-    final static String PROVIDER_LOCATION = "classpath:providers.json";
-    private final ObjectMapper mapper = new ObjectMapper();
-    private final String SPLIT_THE_TOPICS_BY_PLUS = "\\+";
 
-    public List<Provider> providers() {
+    private static final String PROVIDER_LOCATION = "classpath:providers.json";
+    private static final String TOPIC_DELIMITER = "\\+";
+
+    private final ObjectMapper objectMapper;
+    private final ResourceLoader resourceLoader;
+
+    public ProviderLoader(ObjectMapper objectMapper, ResourceLoader resourceLoader) {
+        this.objectMapper = objectMapper;
+        this.resourceLoader = resourceLoader;
+    }
+
+    public List<Provider> getProviders() {
+        Resource resource = resourceLoader.getResource(PROVIDER_LOCATION);
+
         try {
-            File file = ResourceUtils.getFile(PROVIDER_LOCATION);
+            ProvidersWrapper providersWrapper = objectMapper.readValue(resource.getInputStream(), ProvidersWrapper.class);
 
-            ProvidersWrapper wrapper = mapper.readValue(file, ProvidersWrapper.class);
-
-            return wrapper.getProviders().stream()
-                    .map(raw -> new Provider(
-                            raw.getName(),
-                            Set.of(raw.getTopics().split(SPLIT_THE_TOPICS_BY_PLUS)).stream()
-                                    .map(Topic::fromString)
-                                    .collect(Collectors.toSet())))
+            return providersWrapper.getProviders().stream()
+                    .map(providerMapper -> new Provider(
+                            providerMapper.getName(),
+                            parseTopics(providerMapper.getTopics())))
                     .toList();
 
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("Failed to load providers configuration", e);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new IllegalStateException("Failed to load providers configuration", e);
         }
     }
 
-    private static class ProvidersWrapper {
+    private Set<Topic> parseTopics(String topics) {
+        return Set.of(topics.split(TOPIC_DELIMITER)).stream()
+                .map(Topic::fromString)
+                .collect(Collectors.toSet());
+    }
 
+
+    private static class ProvidersWrapper {
         private List<ProviderMapper> providers;
 
         public List<ProviderMapper> getProviders() {
             return providers;
         }
-
-        public void setProviders(List<ProviderMapper> providers) {
-            this.providers = providers;
-        }
     }
 
     private static class ProviderMapper {
-
         private String name;
         private String topics;
 
@@ -61,17 +68,9 @@ public class ProviderLoader {
             return name;
         }
 
-        public void setName(String name) {
-            this.name = name;
-        }
-
         public String getTopics() {
             return topics;
         }
 
-        public void setTopics(String topics) {
-            this.topics = topics;
-        }
     }
-
 }
